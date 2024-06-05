0b5aac33b40c5a29fb3968b827e9b63d298efefb
Pierediaz <pierediaz.putra@gmail.com>
2024-06-05 15:22:45 +0700

    test

---
+++ WebsiteMeraki-VisualStudio/Pages/Galery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteMeraki_VisualStudio.Pages
+{
+    public class GaleryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebsiteMeraki-VisualStudio/Pages/merek.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebsiteMeraki_VisualStudio.Pages
+{
+    public class merekModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
